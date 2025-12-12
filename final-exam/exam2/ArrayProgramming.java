db5f5fef1ef100a20ca6a3312f1f4d2ffbdf76fa
sangvo <stvo1589@gmail.com>
2025-12-12 03:25:02 -0800

    2d programming exercise

--- final-exam/exam2/ArrayProgramming.java
+++ final-exam/exam2/ArrayProgramming.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayProgramming {
+    public static void main(String[] args) {
+        int[][] lis = {{1, 2, 3}, {4, 3, 2, 1}, {6, 7, 7}, {8}};
+
+        System.out.println(numUnique(lis));
+    }
+
+    public static int numUnique(int[][] list) {
+        List<Integer> numsUnique = new ArrayList<>();
+
+        for (int[] nums : list) {
+            for (int num : nums) {
+                if (!numsUnique.contains(num)) {
+                    numsUnique.add(num);
+                }
+            }
+        }
+
+        return numsUnique.size();
+    }
 }
